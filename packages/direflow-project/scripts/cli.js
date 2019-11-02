43f5ed0be8d4e06c07d33c6b6e28e83d0ae1aaef
Silind <silindsoftwaredk@gmail.com>
2019-11-02 16:27:26 +0100

    Enhanced scripts, moved css in components

--- packages/direflow-project/scripts/cli.js
+++ packages/direflow-project/scripts/cli.js
@@ -28,12 +28,20 @@ if (process.argv[2] === 'test-all') {
 }
 
 async function start() {
+  console.log('');
+  console.log(chalk.whiteBright('All Direflow Components will be built and served with the Direflow Project...'));
+  console.log(chalk.whiteBright('Development server will start after this process has finished.'));
+
   await buildAll();
 
   const devServer = spawn('webpack-dev-server', [
     '--config',
     './node_modules/direflow-project/config/config.development.js',
   ]);
+
+  if (process.argv[3] === '--show-output') {
+    devServer.stdout.pipe(process.stdout);
+  }
 
   console.log('');
   console.log(chalk.blueBright(`Starting Direflow Project at ${chalk.magenta('localhost:8000')}`));
@@ -57,6 +65,7 @@ async function start() {
 
   devServer.stderr.on('readable', () => {
     console.log(chalk.red('Direflow Project failed to compile...'));
+    devServer.kill();
     process.exit(1);
   });
 }
