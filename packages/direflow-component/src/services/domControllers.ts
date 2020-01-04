227375ef88929af860a256f4f22f745856b78a9a
Silind <silindsoftwaredk@gmail.com>
2020-01-04 15:03:43 +0100

    Removing dead code + refactoring

--- packages/direflow-component/src/services/domControllers.ts
+++ packages/direflow-component/src/services/domControllers.ts
@@ -1,27 +1,5 @@
-export const getAdditionalChildren = (webComponent: HTMLElement): Element[] => {
-  const allChildren = webComponent.children;
-  return Array.from(allChildren).filter((child) => child.nodeName !== 'SPAN');
-};
-
-export const getAppElement = (webComponent: HTMLElement): Element | undefined => {
-  const allChildren = webComponent.children;
-  const shadowHost = Array.from(allChildren).find((child) => child.nodeName === 'SPAN');
-
-  return shadowHost?.shadowRoot?.children[0];
-};
-
-export const injectIntoFirstChild = (webComponent: HTMLElement, element: Element): void => {
-  const allChildren = webComponent.children;
-  const shadowHost = Array.from(allChildren).find((child) => child.nodeName === 'SPAN');
-
-  shadowHost?.shadowRoot?.children[0]?.prepend(element);
-};
-
 export const injectIntoShadowRoot = (webComponent: HTMLElement, element: Element): void => {
-  const allChildren = webComponent.children;
-  const shadowHost = Array.from(allChildren).find((child) => child.nodeName === 'SPAN');
-
-  shadowHost?.shadowRoot?.prepend(element);
+  webComponent.shadowRoot?.prepend(element);
 };
 
 export const injectIntoHead = (element: Element) => {
--- test/domController.test.ts
+++ test/domController.test.ts
@@ -1,8 +1,5 @@
 import { JSDOM } from 'jsdom';
 import { 
-  getAdditionalChildren,
-  getAppElement,
-  injectIntoFirstChild,
   injectIntoShadowRoot,
   injectIntoHead,
   stripStyleFromHead
@@ -13,62 +10,13 @@ const dom = new JSDOM();
 (global as any).window = dom.window;
 
 const webComponent = document.createElement('div');
-const emptyWebComponent = document.createElement('div');
-const shadowHost = document.createElement('span');
 const appElement = document.createElement('div');
-shadowHost.attachShadow({ mode: 'open' });
-shadowHost.shadowRoot?.append(appElement);
+webComponent.attachShadow({ mode: 'open' });
+webComponent.shadowRoot?.append(appElement);
+
 appElement.id = 'app';
-
-webComponent.append(document.createElement('style'));
-webComponent.append(document.createElement('script'));
-webComponent.append(shadowHost);
-
-describe('Get Additional Children', () => {
-  it('should get correct number of additional children', () => {
-    const elements = getAdditionalChildren(webComponent);
-    expect(elements.length).toBe(2);
-  });
-
-  it('should get correct first additional children', () => {
-    const elements = getAdditionalChildren(webComponent);
-    expect(elements[0].nodeName).toBe('STYLE');
-  });
-
-  it('should get correct second additional children', () => {
-    const elements = getAdditionalChildren(webComponent);
-    expect(elements[1].nodeName).toBe('SCRIPT');
-  });
-});
-
-describe('Get App Element', () => {
-  it('should get an element if exists', () => {
-    const element = getAppElement(webComponent);
-    expect(element).toBeTruthy();
-  });
-
-  it('should get undefined if not exists', () => {
-    const element = getAppElement(emptyWebComponent);
-    expect(element).toBeUndefined();
-  });
-
-  it('should get correct app element', () => {
-    const element = getAppElement(webComponent);
-    expect(element?.id).toBe('app');
-  });
-});
-
-describe('Inject into first child', () => {
-  it('should correctly into first child', () => {
-    const element = document.createElement('div');
-    element.id = 'injected_first_child';
-
-    injectIntoFirstChild(webComponent, element);
-    const appElement = getAppElement(webComponent);
-
-    expect(appElement?.children[0]?.id).toBe('injected_first_child');
-  });
-});
+appElement.append(document.createElement('style'));
+appElement.append(document.createElement('script'));
 
 describe('Inject into Shadow Root', () => {
   it('should correctly inject into Shadow Root', () => {
@@ -77,7 +25,7 @@ describe('Inject into Shadow Root', () => {
 
     injectIntoShadowRoot(webComponent, element);
 
-    expect(shadowHost.shadowRoot?.children[0]?.id).toBe('injected_shadow');
+    expect(webComponent.shadowRoot?.children[0]?.id).toBe('injected_shadow');
   });
 });
 
