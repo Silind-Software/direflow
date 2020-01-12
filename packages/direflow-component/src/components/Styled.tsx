5e2bbbcae14b89c93e4a7ad171be228951bed104
Silind <silindsoftwaredk@gmail.com>
2020-01-12 18:09:05 +0100

    Support providing multiple styles

--- packages/direflow-component/src/components/Styled.tsx
+++ packages/direflow-component/src/components/Styled.tsx
@@ -2,16 +2,27 @@ import React, { FC, Component, ReactNode, ComponentClass, CSSProperties } from '
 import Style from 'style-it';
 
 interface IStyled {
-  styles: CSSProperties;
+  styles: CSSProperties | CSSProperties[];
   children: ReactNode | ReactNode[];
 }
 
 const Styled: FC<IStyled> = (props): JSX.Element => {
-  const styles = props.styles.toString();
+  let styles;
+
+  if (typeof props.styles === 'string') {
+    styles = (props.styles as CSSProperties).toString();
+  } else {
+    styles = (props.styles as CSSProperties[]).reduce(
+      (acc: CSSProperties, current: CSSProperties) => `${acc} ${current}` as CSSProperties,
+    );
+  }
+
   return Style.it(styles, props.children);
 };
 
-const withStyles = (styles: CSSProperties) => <P, S>(WrappedComponent: ComponentClass<P, S> | FC<P>) => {
+const withStyles = (styles: CSSProperties | CSSProperties[]) => <P, S>(
+  WrappedComponent: ComponentClass<P, S> | FC<P>,
+) => {
   return class extends Component<P, S> {
     public render(): JSX.Element {
       return (
