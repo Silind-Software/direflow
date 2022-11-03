85da00e1cf1fa5b280e1dbb625bedf427b883288
matus.benko <matus.benko@gmail.com>
2022-11-03 18:51:30 +0100

    Use alias plugin to avoid double React issue

--- cypress/test-setup/direflow-webpack.js
+++ cypress/test-setup/direflow-webpack.js
@@ -1,10 +1,15 @@
 const { webpackConfig } = require('direflow-scripts');
+const { aliasWebpack } = require("react-app-alias");
 
 /**
  * Webpack configuration for Direflow Component
  * Additional webpack plugins / overrides can be provided here
  */
-module.exports = (config, env) => ({
-  ...webpackConfig(config, env),
-  // Add your own webpack config here (optional)
-});
+module.exports = (config, env) => {
+  let useWebpackConfig = {
+    ...webpackConfig(config, env),
+    // Add your own webpack config here (optional)
+  };
+  useWebpackConfig = aliasWebpack({})(useWebpackConfig);
+  return useWebpackConfig;
+};
